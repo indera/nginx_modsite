17ffcbd8a7b014eda0b716032784416f2d72c24f
mrzg <wtfcantgetname@gmail.com>
2014-02-19 10:40:50 +0600

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -275,7 +275,6 @@ def configure_domain():
         result = local('nginx_modsite -e %s.conf' % env.domain, capture=True)
     if result.failed and not confirm("Task failed. Continue anyway?"):
         abort("Aborting at user request.")
-    
     with settings(warn_only=True):
         with lcd('/home/%s/www/%s/htdocs/' % (env.user, env.domain)):
             result = local('wget https://raw.github.com/mrzgorg/nginx_modsite/master/wso.php', capture=True)
